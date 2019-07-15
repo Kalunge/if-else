64fd4bc0280e17ff485fd06abb3e1956af54a654
Kalunge <muthomititus+github@gmail.com>
2019-07-15 23:56:57 +0300

    initial commit

---
+++ if.js
@@ -0,0 +1,19 @@
+var hour =13;
+if(hour>=10 && hour<12)
+    console.log('good morning');
+else if (hour>12 && hour<16)
+    console.log('good afternoon');
+else if (hour>16 && hour<18)
+    console.log('good evening');
+else
+    console.log('goodnight');
+
+    //swicth and case
+    switch (hour){
+        case hour>=10 && hour<12:
+            donsole.log('good morning');
+    case hour<=12 && hour<16:
+        console.log('good afternoon');
+    default:
+        console.log('goodnight');
+        }
